86b499bb371fae776cb1200670af161285ec94c7
orEkronot <orekronot@gmail.com>
2026-01-22 16:30:14 +0200

    update

--- 02_sync/07_conditional variable.cc
+++ 02_sync/07_conditional variable.cc
@@ -12,34 +12,6 @@
 std::deque<int> q;
 std::mutex mu;
 
-void function_1() {
-	int count = 10;
-	while (count > 0) {
-		std::unique_lock<mutex> locker(mu);
-		q.push_front(count);
-		locker.unlock();
-		std::this_thread::sleep_for(chrono::seconds(1));
-		count--;
-	}
-}
-
-void function_2() {
-	int data = 0;
-	while ( data != 1) {
-		std::unique_lock<mutex> locker(mu);
-		if (!q.empty()) {
-			data = q.back();
-			q.pop_back();
-			locker.unlock();
-			cout << "t2 got a value from t1: " << data << endl;
-		} else {
-			locker.unlock();
-		}
-		std::this_thread::sleep_for(chrono::milliseconds(10));
-	}
-}
-
-
 // Using conditional variable and mutex
 void function_1() {
 	int count = 10;
@@ -74,15 +46,5 @@ int main() {
 	t2.join();
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
  
  
